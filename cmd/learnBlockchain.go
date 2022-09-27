9944326f00d5cdb0efe787ab827fb7cbea213d96
gtkim <gtkim@netcus.com>
2022-09-27 16:59:10 +0900

    하단계 문제풀이를 위한 난이도 필터링

--- cmd/learnBlockchain.go
+++ cmd/learnBlockchain.go
@@ -31,15 +31,25 @@ func learnBlockchainSelect() string {
 		score := data.GetUserScore()
 		level := levelConverter(int(score))
 		fmt.Println("Your score is ", score, "Let's start ", level, " Lecture!")
-		switch level {
-		case global.Beginner:
-			fmt.Println("go beginner step")
-		case global.Intermediate:
-			fmt.Println("go intermediate step")
-		case global.Advanced:
-			fmt.Println("go advanced step")
-		}
+
+		LearnBlockchain(level)
 	}
 
 	return result
+}
+
+func LearnBlockchain(level string) {
+	switch level {
+	case global.Beginner:
+		fmt.Println("go beginner step")
+		//하단계 교육 로직
+		//한단계 종료 후
+		CreateQuestionBank(global.LevelTestQuestionCount, 0)
+	case global.Intermediate:
+		fmt.Println("go intermediate step")
+		CreateQuestionBank(global.LevelTestQuestionCount, 1)
+	case global.Advanced:
+		fmt.Println("go advanced step")
+		CreateQuestionBank(global.LevelTestQuestionCount, 2)
+	}
 }
